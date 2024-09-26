59ad9ceedb50bb7aeb54d5dea665dbd1f4c95a88
akashvenus <akashvenu15@gmail.com>
2024-09-26 19:56:35 -0400

    Chatbot can remember history

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from langchain_google_genai import GoogleGenerativeAI
+# from langchain_google_genai import GoogleGenerativeAI
+import google.generativeai as genai
+import re
 
 st.title("Chatbot")
 
-llm = GoogleGenerativeAI(model="gemini-1.0-pro", google_api_key=st.secrets['GOOGLE_API_KEY'])
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+
+llm = genai.GenerativeModel("gemini-1.5-flash")
+
 
 #helper function to display and send streamlit messages
-def llm_function(bin_switch:int,query:str):
+def display_message(bin_switch:int,query:str):
 
     if bin_switch % 2 == 0:
         st.session_state.messages.append({"role":"user","content":query})
@@ -23,16 +28,30 @@ def llm_function(bin_switch:int,query:str):
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if len(st.session_state.messages) == 0:
-    initial_prompt = "Introduce yourself as Dox, an assistant powered by Google Gemini. You use emojis to be interactive and be funny."   
-    message = llm.invoke(initial_prompt)
-    llm_function(1,message) 
+    initial_prompt = '''Introduce yourself as Dox, an assistant powered by Google Gemini. You have to collect only the following information 
+    from the user initially:
+    - Age
+    - Current Gender (Male, Female or Non-binary)
+    - Gender assigned at birth (Male or Female)
+    - Whether they are member of public or a health professional
+
+    If the user does not give the above information or asks anything irrelevant, refuse politely and ask for the details again.
+    '''  
+    message = llm.generate_content(initial_prompt)
+    display_message(1,message.candidates[0].content.parts[0].text) 
 
 if prompt := st.chat_input("What's up?"):
-    llm_function(0,prompt)
-    message = llm.invoke(prompt)
-    llm_function(1,message)
+    display_message(0,prompt)
+    
+    conversation_history = "\n".join([f"{msg['role']}: {msg['content']}" for msg in st.session_state.messages])
+    full_prompt = conversation_history + "\nuser: " + prompt
+
+    message = llm.generate_content(full_prompt)
+    display_message(1,message.candidates[0].content.parts[0].text)
