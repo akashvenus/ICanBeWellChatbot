9509c879b45cf83103eb834364c121a9a8ce429e
2091044 <2091044>
2024-10-31 18:10:38 -0400

    updated rag_model.py

--- rag_model.py
+++ rag_model.py
@@ -5,18 +5,38 @@ from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 import os
 from dotenv import load_dotenv
 from collections import OrderedDict
+import json
 
 # Load environment variables from .env file
 load_dotenv()
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-
+# Load the document
 loader = JSONLoader(
-        file_path='data/Topics-EN.json',
-        jq_schema='.',
+        file_path='data/HtmlTopic-EN-final.json',
+        jq_schema='.[] | {button: .Button, topic_heading: ."Topic heading", subject: .Subject, general_patient_text: ."General Patient Text", health_provider_text: ."Health Provider Text", gender: .Gender, min_age: ."Minimum age", max_age: ."Maximum age"}',
         text_content=False)
 docs = loader.load()
+
+#  With metadata
+for doc in docs:
+    page_content = json.loads(doc.page_content)
+    doc.metadata.update({
+    'button': page_content['button'],
+    'topic_heading': page_content['topic_heading'],
+    'gender': page_content['gender'],
+    'min_age': page_content['min_age'],
+    'max_age': page_content['max_age']
+        })
+    del page_content['button']
+    del page_content['topic_heading']
+    del page_content['gender']
+    del page_content['min_age']
+    del page_content['max_age']
+    doc.page_content = json.dumps(page_content)
+print(docs)
+
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=35)
 texts = text_splitter.split_documents(docs)
@@ -31,11 +51,12 @@ try:
 except Exception as e:
     print(f"Error creating vector embeddings: {e}")
 
+
 # Initialize Chroma vector store
 vector_store = Chroma(embedding_function=embedding_model, persist_directory="data")
 
-# Add documents to the vector store
-vector_store.add_documents(documents=texts)
+# Add documents to the vector store 
+vector_store.add_documents(documents=texts) #Correction
 
 # Validate the setup
 try:
