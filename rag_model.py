5a323e839e03d422d4db6d324c9f7abcf086fd42
2091044 <2091044>
2024-10-31 17:03:03 -0400

    Updated rag_model.py

--- rag_model.py
+++ rag_model.py
@@ -18,19 +18,9 @@ loader = JSONLoader(
         text_content=False)
 docs = loader.load()
 
-# Load Semantics data
-# loader_semantics = JSONLoader(
-#     file_path='data/Semantics.json',
-#     jq_schema='.',
-#     text_content=False
-# )
-# semdocs = loader_semantics.load()
-
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=35)
 texts = text_splitter.split_documents(docs)
 
-#semantics data
-# semantics_texts = text_splitter.split_documents(semdocs)
 
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
@@ -41,24 +31,11 @@ try:
 except Exception as e:
     print(f"Error creating vector embeddings: {e}")
 
-# Convert semantics to embeddings
-# try:
-#     semantics_embeddings = embedding_model.embed_documents([doc.page_content for doc in semantics_texts])
-#     print("Vector embeddings for Semantics created successfully")
-# except Exception as e:
-#     print(f"Error creating vector embeddings for Semantics: {e}")
-
 # Initialize Chroma vector store
 vector_store = Chroma(embedding_function=embedding_model, persist_directory="data")
 
 # Add documents to the vector store
 vector_store.add_documents(documents=texts)
-
-# # Initialize Chroma vector store for Semantics
-# vector_store_semantics = Chroma(embedding_function=embedding_model, persist_directory="data/semantics")
-
-# # Add documents to the semantics vector store
-# vector_store_semantics.add_documents(documents=semantics_texts)
 
 # Validate the setup
 try:
@@ -78,9 +55,4 @@ try:
 except Exception as e:
     print(f"Error during test query: {e}")
 
-    # navigation_query = input("Would you like to know how to navigate within the app? (yes/no): ")
     
-    # if navigation_query.lower() == "yes":
-    #     # Fetch navigation details using the same query
-    #     results_semantics = vector_store_semantics.search(query=test_query, search_type='similarity')
-
